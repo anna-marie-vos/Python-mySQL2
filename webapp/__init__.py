eb300e69383e3f1b0963ffaf6ac17f2b0bf784f1
anna-marie-vos <amvos@outlook.com>
2017-04-02 15:28:02 +1200

    adding template

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return Response("hello"), 200
+    return render_template('index.html'), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
